d5a44b5459a589711715d74b4925d57298f75da7
upeh <wisdomupeh@gmail.com>
2026-06-21 01:08:47 +0100

    added chromium

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
+    executablePath: "/usr/bin/chromium",
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
